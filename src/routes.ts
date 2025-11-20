6aae75b7bd14083e337a1aae03e8bc4764bcfc3e
michael-p37 <michaelp_99@outlook.com>
2025-11-20 15:57:46 -0500

    feat: create users.routes.ts & register in routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRoutes from "./routes/auth.routes.js";
 import categoriesRoutes from "@/routes/categories.routes.js";
 import productsRoutes from "@/routes/products.routes.js";
+import usersRoutes from "@/routes/users.routes.js";
 
 const router = Router();
 
 router.use("/api/", authRoutes);
 router.use("/api/categories", categoriesRoutes);
 router.use("/api/products", productsRoutes);
+router.use("/api/profile", usersRoutes);
 
 export default router;
