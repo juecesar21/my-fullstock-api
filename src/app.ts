e02d8a62ee1c4355082b539bbdf936bf2c2c5de4
michael-p37 <michaelp_99@outlook.com>
2026-04-09 17:54:38 -0500

    feat: cors configuratio

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,19 @@ import {
 } from "@/middlewares/error.middleware.js";
 import sessionMiddleware from "@/middlewares/session.middleware.js";
 import routes from "@/routes.js";
+import cors from "cors";
+// import { corsMiddleware } from "./middlewares/cors.middleware.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(morgan("dev"));
 app.use(express.json());
